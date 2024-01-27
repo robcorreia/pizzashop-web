1ab31563775c823c550ac7e95968cfde1c0c8017
Robson <robcorreia@gmail.com>
2024-01-27 12:34:40 -0300

    feat: add orders page + list orders

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 
@@ -15,6 +16,7 @@ export const router = createBrowserRouter([
         path: '/',
         element: <Dashboard />,
       },
+      { path: '/orders', element: <Orders /> },
     ],
   },
   {
